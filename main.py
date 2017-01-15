e205d0954a159f887d543be11d3ccc2d60a49638
LeoCo <leonardo.cotti@gmail.com>
2017-01-15 01:45:56 +0100

    Last Version

--- main.py
+++ main.py
@@ -1,19 +1,20 @@
 from getdata import get_banks_data
 import matplotlib.pyplot as plt
+import statsmodels.api as sm
 
 banks = get_banks_data()
 
 for x in banks:
 
     #Stampa lista banche
-    if True:
+    if False:
         print(x.name)
         print(x.ticker)
         print(x.datas.head())
         print(x.prices.head())
 
     #Fa il grafico del prezzo delle azioni
-    if True:
+    if False:
         df = x.prices
         plt.plot(df['Date'], df['PX_LAST'])
         plt.title(x.name)
@@ -21,6 +22,27 @@ for x in banks:
         plt.ylabel('Prices')
         plt.show()
 
+    #Regressione
+    if True:
 
+        y = x.prices['PX_LAST']
+
+        X = list(range(0,len(x.prices['PX_LAST'])))
+        X = sm.add_constant(X)
+
+        model = sm.OLS(y, X)
+        results = model.fit()
+        print(results.summary())
+
+        predictions = results.predict(X)
+
+        df = x.prices
+        plt.plot(df['Date'], df['PX_LAST'], label="Sample")
+        plt.plot(df['Date'],predictions, label="Prediction")
+        plt.title(x.name)
+        plt.legend()
+        plt.xlabel('Date')
+        plt.ylabel('Prices')
+        plt.show()
 
 
