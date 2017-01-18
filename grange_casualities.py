d6c787c1e248e17b626be34c22225cf2991f0831
LeoCo <leonardo.cotti@gmail.com>
2017-01-19 00:59:50 +0100

    Last Version

--- grange_casualities.py
+++ grange_casualities.py
@@ -4,7 +4,7 @@ import statsmodels.tsa.stattools as st
 from getdata import get_banks_data
 
 
-def granger_casualties(banks):
+def granger_casualties(banks, full_period=True, date_start="", date_end=""):
     banks_name = []
 
     #Creo la matrice delle correlazioni
@@ -13,18 +13,18 @@ def granger_casualties(banks):
 
     corr_matrix = pd.DataFrame(index=banks_name, columns=banks_name)
 
-    for x in corr_matrix.columns:
-        for y in corr_matrix.index:
+    for x in corr_matrix.index:
+        for y in corr_matrix.columns:
 
             bank_a = bank.find_ticker_in_list(x,banks)
             bank_b = bank.find_ticker_in_list(y,banks)
 
-            corr_matrix.loc[x,y] = correlation_a_granger_caused_by_b(bank_a,bank_b)
+            corr_matrix.loc[x,y] = correlation_a_granger_caused_by_b(bank_a,bank_b,full_period=full_period, date_start=date_start, date_end=date_end)
 
 
     return corr_matrix
 
-def correlation_a_granger_caused_by_b(a, b, full_period=True, date_start="2010-1-1", date_end="2011-1-1",p_value=0.01):
+def correlation_a_granger_caused_by_b(a, b, full_period=True, date_start="", date_end="",p_value=0.01):
 
     influenced = 0
 
@@ -38,6 +38,9 @@ def correlation_a_granger_caused_by_b(a, b, full_period=True, date_start="2010-1
         #Filtro solo le date che mi servono
         mask = (yeald_inner_join['Date'] >= date_start) & (yeald_inner_join['Date'] <= date_end)
         yeald_inner_join = yeald_inner_join.loc[mask]
+        #Se non ci sono sovrapposizioni di date ritorna zero ovvero che non sono correlate
+        if yeald_inner_join.empty:
+            return 0
 
 
     x = yeald_inner_join[['Yeald_x','Yeald_y']]
@@ -55,6 +58,31 @@ if __name__ == '__main__':
 
     banks = get_banks_data()
 
-    granger_matrix = granger_casualties(banks)
+    writer = pd.ExcelWriter('Output/granger_casualties_test.xlsx')
 
+    print('Granger Matrix of the 2008 crysis')
+    date_start = '2006-9-15'
+    date_end = '2008-9-15'
+    granger_matrix = granger_casualties(banks, full_period=False, date_start=date_start, date_end=date_end)
     print(granger_matrix)
+    print()
+    sheet_title = str(date_start) + "_" + str(date_end)
+    granger_matrix.to_excel(writer, sheet_title)
+
+    print('Granger Matrix of the 2012 crysis')
+    date_start = '2010-6-30'
+    date_end = '2012-6-30'
+    granger_matrix = granger_casualties(banks, full_period=False, date_start=date_start, date_end=date_end)
+    print(granger_matrix)
+    print()
+    sheet_title = str(date_start) + "_" + str(date_end)
+    granger_matrix.to_excel(writer, sheet_title)
+
+    print('Granger Matrix of the full available time period')
+    granger_matrix = granger_casualties(banks)
+    print(granger_matrix)
+    print()
+    sheet_title = "Full Period"
+    granger_matrix.to_excel(writer, sheet_title)
+
+    writer.save()
