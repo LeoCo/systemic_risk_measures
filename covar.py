42ea9a16e54de6e7a75876b557dac0fb31695883
LeoCo <leonardo.cotti@gmail.com>
2017-01-19 23:18:17 +0100

    Last Version

--- covar.py
+++ covar.py
@@ -1,5 +1,6 @@
 from getdata import get_banks_data
 import pandas as pd
+import numpy as np
 import time
 
 
@@ -9,31 +10,58 @@ if __name__ == '__main__':
 
     banks = get_banks_data()
 
-    bank = banks[0]
-
-    print(bank.mva)
-
     print("Get data time: " + str(time.clock() - start))
 
-    year_start = 2006
-    quarter_start = 0
+    year_start = 1997
+    quarter_start = 1
 
-    year_end = 2009
-    quarter_end = 4
+    year_end = 2015
+    quarter_end = 5
 
     portfolio_system_return = pd.DataFrame(columns=['Year','Quarter','PSR'])
 
     for year in range(year_start,year_end):
         for quarter in range(quarter_start,quarter_end):
 
+            numerator = 0
 
-            entry = pd.Series([2001, 1, 50])
-            #portfolio_system_return.append([2001, 1, 50])
+            denominator = 0
+
+            for bank in banks:
+
+                mask = (bank.mva['Year'] == year) & (bank.mva['Quarter'] == quarter)
+
+                #print("Bank name: " + bank.name)
+                #print(bank.mva.head())
+                #print(bank.mva['MVA'][mask])
+                #print(bank.mva[mask].empty)
+                #print('ok')
+
+                delta_mva = 0
+                mva = 0
+
+                if bank.mva[mask].empty:
+                    mva = 0
+                    delta_mva = 0
+                else:
+                    mva = np.float64(bank.mva['MVA'][mask])
+                    delta_mva = np.float64(bank.mva['DELTA_MVA'][mask])
+
+                numerator += mva * delta_mva
+
+                denominator += mva
+
+
+
+            current_portfolio_system_return = numerator / denominator
+
+            entry = pd.Series([year, quarter, current_portfolio_system_return], index=['Year','Quarter','PSR'])
+            portfolio_system_return = portfolio_system_return.append(entry, ignore_index=True)
 
     print(portfolio_system_return)
-
 
 
     run_time = time.clock() - start
 
     print("Execution time: " + str(run_time))
+
